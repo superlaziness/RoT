68d0393c5953f436108ffe3524934154a5718a68
erend <gleb.erend@gmail.com>
2017-01-17 21:17:29 +0300

    telegram botrepaired

--- src/rot/node/telegrambot.js
+++ src/rot/node/telegrambot.js
@@ -16,7 +16,7 @@ export default function telegramBot(getValue, getList, setValue) {
       ? command = messageText
       : command = messageText.substring(0, commandEnd); // geting command from user message
 
-    const helpMessage = '/get "[name]" - get value.\n/list - list of existing things'; // /help command message
+    const helpMessage = '/get "[name]" - get value.\n/list - list of existing things.'; // /help command message
     const wtf = 'wtf?!';                               // not a command message
     const notExist = "this thing doesn't exist.";      // thing doesn't exist message
 
@@ -41,7 +41,7 @@ export default function telegramBot(getValue, getList, setValue) {
         bot.sendMessage(chatId, helpMessage);
         break;
       case '/get':
-        const name = messageText.substring(5);         // getting name of the thing from user message
+        const name = messageText.substring(commandEnd);         // getting name of the thing from user message
         sendValue(name);
         break;
       case '/list':
