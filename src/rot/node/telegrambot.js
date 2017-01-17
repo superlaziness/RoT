32a48f3d0e2fa7e8325b5877ab0c7bb6723542e4
erend <gleb.erend@gmail.com>
2017-01-17 20:22:45 +0300

    telegram bot upgraded

--- src/rot/node/telegrambot.js
+++ src/rot/node/telegrambot.js
@@ -9,17 +9,35 @@ export default function telegramBot(getValue) {
   bot.on('message', (msg) => {
     const chatId = msg.chat.id;
     const messageText = msg.text;
+
+    const commandEnd = messageText.indexOf(" ");
+    const command = messageText.substring(0, commandEnd);        // geting command from user message
+console.log(command);
+    const helpMeassage = '/get "[name]" - get value.'; // /help command message
+    const wtf = 'wtf?!';                               // not a command message
+    const notExist = "this thing doesn't exist.";      // thing doesn't exist message
+
     const searchStore = function(thing) {
       return getValue(thing);
     }
 
-    if(messageText === '/help') {
-      bot.sendMessage(chatId, '"[name]" - get value.');
-    } else {
-      searchStore(msg.text) 
-        ? bot.sendMessage(chatId, searchStore(msg.text))
-        : bot.sendMessage(chatId, 'what the huynya you just wrote?!');
-    };
-  })
-  
+    const sendValue = function(name) {
+      searchStore(name) 
+        ? bot.sendMessage(chatId, searchStore(name))
+        : bot.sendMessage(chatId, notExist);
+    }
+
+    switch(command) {
+      case '/help':
+        bot.sendMessage(chatId, helpMeassage);
+        break;
+      case '/get':
+        const name = messageText.substring(5);         // getting name of the thing from user message
+        sendValue(name);
+        break;
+      default:
+        const wtf = 'wtf?!'
+        bot.sendMessage(chatId, wtf);
+    }
+  })  
 };
