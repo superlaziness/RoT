ebe53d9033ab114bcbf321d1eb5b32823883b126
Stanislav Shemyakin <shemyakin@me.com>
2017-01-19 01:39:47 +0300

    made React.render work on server

--- src/server/index.jsx
+++ src/server/index.jsx
@@ -1,35 +1,55 @@
 import Express from "express";
-import React from "react";
 import { Provider } from "react-redux";
 import { renderToString, renderToStaticMarkup } from "react-dom/server";
 import path from "path";
+import { jsdom } from 'jsdom';
 
 import configureStore from "store";
 
 import Maco from "components/maco";
 import Html from 'components/html';
 
-const app = new Express();
+//Configure Redux store
 const store = configureStore();
 
-let children = '<div>empty</div>';
-
-const renderApp = () => {
-  children = renderToString(
-    <Provider store={store}>
-      <Maco/>
-    </Provider>
-  );
+//Prepare jsdom env
+const htmlString = '<html><body><div id="app"></div></body></html>';
+global.document = jsdom(htmlString);
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    global[property] = document.defaultView[property];
+  }
+});
+global.navigator = {
+  userAgent: 'node.js'
 };
 
+//Require React after window & document defined
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+//Run app
+ReactDOM.render(
+  <Provider store={store}>
+    <Maco/>
+  </Provider>,
+  document.getElementById('app')
+);
+
+//Run webserver
+const app = new Express();
 const isDev = process.env.NODE_ENV === 'development';
 const scriptUrl = isDev ? 'http://localhost:8080/dist/client.js' : '/static/client.js';
-
-store.subscribe(renderApp);
 
 app.use("/static", Express.static('./dist'));
 
 app.get("*", (req, res) => {
+  const children = renderToString(
+    <Provider store={store}>
+      <Maco/>
+    </Provider>
+  );
 	const markup = renderToStaticMarkup(
 		<Html 
       children={children}
@@ -40,7 +60,5 @@ app.get("*", (req, res) => {
 
 	res.status(200).send(`<!doctype html>${markup}`);
 });
-
-renderApp();
 
 app.listen(10000, () => {console.log("server is running!")});
