6ddf5b587055a43a46b74a15ad35d21c35fef372
erend <gleb.erend@gmail.com>
2017-01-18 18:27:50 +0300

    telegram bot now works with objects

--- src/rot/node/telegrambot.js
+++ src/rot/node/telegrambot.js
@@ -10,29 +10,29 @@ export default function telegramBot(getValue, getList, setValue) {
     const chatId = msg.chat.id;
     const messageText = msg.text;
 
-    const commandEnd = messageText.indexOf(" ");
-    let command;
-    commandEnd === -1 
-      ? command = messageText
-      : command = messageText.substring(0, commandEnd); // geting command from user message
-
     const helpMessage = '/get "[name]" - get value.\n/list - list of existing things.'; // /help command message
     const wtf = 'wtf?!';                               // not a command message
     const notExist = "this thing doesn't exist.";      // thing doesn't exist message
     const errorMesage = "error";                       // error message
 
 
+    const commandEnd = messageText.indexOf(" ");
+    let command;
+    commandEnd === -1 
+      ? command = messageText
+      : command = messageText.substring(0, commandEnd); // geting command from user message
+
     const searchStore = function(thing) {
       return getValue(thing);
     }
 
     const sendValue = function(name) {
-      console.log(searchStore(name));
-      if (searchStore(name) !== undefined) {
-        if(typeof searchStore(name) === 'object') {
-          sendMessage(JSON.stringify(obj));
+      const thingValue = searchStore(name);
+      if (thingValue !== undefined) {
+        if(typeof thingValue === 'object') {
+          sendMessage(JSON.stringify(thingValue));
         } else {
-          sendMessage(searchStore(name));
+          sendMessage(thingValue);
         }
       } else {
         sendMessage(notExist);
@@ -56,7 +56,7 @@ export default function telegramBot(getValue, getList, setValue) {
         sendMessage(helpMessage);
         break;
       case '/get':
-        const name = messageText.substring(commandEnd);         // getting name of the thing from user message
+        const name = messageText.substring(commandEnd + 1);         // getting name of the thing from user message
         sendValue(name);
         break;
       case '/list':
